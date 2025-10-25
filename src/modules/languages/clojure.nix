94e4a138c68b1cb5131ded9320cbfea640edc28e
SZanko <szanko_dev@proton.me>
2025-10-26 00:35:55 +0100

    Add leinigen support to devenv
    Signed-off-by: SZanko <szanko_dev@proton.me>

--- src/modules/languages/clojure.nix
+++ src/modules/languages/clojure.nix
@@ -2,10 +2,22 @@
 
 let
   cfg = config.languages.clojure;
+  inherit (lib) types mkEnableOption mkOption mkDefault mkIf optional literalExpression;
 in
 {
   options.languages.clojure = {
     enable = lib.mkEnableOption "tools for Clojure development";
+    leinigen = {
+      enable = mkEnableOption "leinigen";
+      package = mkOption {
+        type = types.package;
+        defaultText = literalExpression "pkgs.leinigen.override { jdk_headless = cfg.jdk.package; }";
+        description = ''
+          The leinigen package to use.
+          The leinigen package by default inherits the JDK from `languages.java.jdk.package`.
+        '';
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -14,7 +26,9 @@ in
         jdk = config.languages.java.jdk.package;
       })
       clojure-lsp
-    ];
+
+    ]
+    ++ (optional cfg.leinigen.enable cfg.leinigen.package);
     languages.java.enable = true;
   };
 }
